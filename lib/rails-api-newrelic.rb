26b3dd19874b29739f1fea8f7f64745ed7501e51
Michael Gee <michaelpgee@gmail.com>
2014-01-16 17:17:39 -0500

    implement fix suggested by rails-api/rails-api#34

--- lib/rails-api-newrelic.rb
+++ lib/rails-api-newrelic.rb
@@ -1,1 +1,12 @@
 require "rails-api-newrelic/version"
+
+require "new_relic/agent/instrumentation/rails3/action_controller"
+require "new_relic/agent/instrumentation/rails3/errors"
+
+class ActionController::API
+
+  include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+  include NewRelic::Agent::Instrumentation::Rails3::ActionController
+  include NewRelic::Agent::Instrumentation::Rails3::Errors
+
+end
